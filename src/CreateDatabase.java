5e8bf1e3c7891d6b43abd8864442fcade39c12f3
s3warriors <satveerswami982@gmail.com>
2025-01-03 14:44:09 +0530

    Added: UC 13: Ensure Retrieve Queries Work with New Table Structure

--- src/CreateDatabase.java
+++ src/CreateDatabase.java
@@ -1,5 +1,7 @@
-// UC 1: Create Address Book Database
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class CreateDatabase {
     public static void main(String[] args) {
